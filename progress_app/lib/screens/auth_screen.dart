fd497dc2e9caf97a970cf9cb7417bf234ee727e6
Neli Todorova <nelci592@gmail.com>
2020-02-10 08:57:23 +0200

    styling customization

--- progress_app/lib/screens/auth_screen.dart
+++ progress_app/lib/screens/auth_screen.dart
@@ -13,8 +13,7 @@ class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
-    // final transformConfig = Matrix4.rotationZ(-8 * pi / 180);
-    // transformConfig.translate(-10.0);
+
     return Scaffold(
       // resizeToAvoidBottomInset: false,
       body: Stack(
@@ -23,10 +22,10 @@ class AuthScreen extends StatelessWidget {
             decoration: BoxDecoration(
               gradient: LinearGradient(
                 colors: [
-                  Color.fromRGBO(152, 251, 152, 1).withOpacity(0.5),
-                  Color.fromRGBO(46,139,87, 1).withOpacity(0.9),
+                  Color.fromRGBO(255, 255, 255, 1),
+                  //  Color.fromRGBO(46,139,87, 1).withOpacity(0.9),
                 ],
-                begin: Alignment.topLeft,
+                begin: Alignment.topRight,
                 end: Alignment.bottomRight,
                 stops: [0, 1],
               ),
@@ -42,28 +41,14 @@ class AuthScreen extends StatelessWidget {
                 children: <Widget>[
                   Flexible(
                     child: Container(
-                      margin: EdgeInsets.only(bottom: 20.0),
-                      padding:
-                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 94.0),
-                     // transform: Matrix4.rotationZ(-8 * pi / 180)
-                     //   ..translate(-10.0),
-                      // ..translate(-10.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color:  Color.fromRGBO(100, 251, 152, 1).withOpacity(0.5),
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 8,
-                            color: Colors.black26,
-                            offset: Offset(0, 2),
-                          )
-                        ],
-                      ),
                       child: Text(
-                        'Progress',
+                        'Login',
+                        textAlign: TextAlign.left,
                         style: TextStyle(
-                          color: Color.fromRGBO( 255,255, 255, 1).withOpacity(0.5),                          fontSize: 50,
-                          fontFamily: 'Anton',
+                          color:
+                              Colors.black,
+                          fontSize: 40,
+                          fontFamily: 'Segoe UI',
                           fontWeight: FontWeight.normal,
                         ),
                       ),
@@ -145,23 +130,21 @@ class _AuthCardState extends State<AuthCard> {
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
     return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      elevation: 8.0,
       child: Container(
-        height: _authMode == AuthMode.Signup ? 320 : 260,
+        height: _authMode == AuthMode.Signup ? 540 : 380,
         constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 540 : 380),
+        width: deviceSize.width,
+        padding: EdgeInsets.all(26.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
             child: Column(
               children: <Widget>[
                 TextFormField(
-                  decoration: InputDecoration(labelText: 'E-Mail'),
+                  decoration: InputDecoration(
+                    labelText: 'E-Mail',
+                     ),
                   keyboardType: TextInputType.emailAddress,
                   validator: (value) {
                     if (value.isEmpty || !value.contains('@')) {
@@ -204,25 +187,43 @@ class _AuthCardState extends State<AuthCard> {
                 if (_isLoading)
                   CircularProgressIndicator()
                 else
-                  RaisedButton(
-                    child:
-                        Text(_authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
-                    onPressed: _submit,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
+                  Container(
+                    height: 50.0,
+                    child: RaisedButton(
+                      onPressed: _submit,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                      padding: EdgeInsets.all(0.0),
+                      child: Ink(
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color.fromRGBO(111,198,25,1), 
+                                 Color.fromRGBO(159, 235, 83, 1)],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                            ),
+                            borderRadius: BorderRadius.circular(10.0)),
+                        child: Container(
+                          constraints:
+                              BoxConstraints(maxWidth: 400.0, minHeight: 50.0),
+                          alignment: Alignment.center,
+                          child:  Text(_authMode == AuthMode.Login ? 'Log in' : 'Sign up',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
                     ),
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
-                    color: Color.fromRGBO(152,251,152,1),
-                    textColor: Theme.of(context).primaryTextTheme.button.color,
                   ),
+
                 FlatButton(
                   child: Text(
                       '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
                   onPressed: _switchAuthMode,
-                  padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
+                  padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 4),
                   materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  textColor: Color.fromRGBO(146,139,87,1),
+                  textColor: Color.fromRGBO(146, 139, 87, 1),
                 ),
               ],
             ),
