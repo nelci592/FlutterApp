29348b94892c26fcab9ad780952de2cd490f77ad
Neli Todorova <nelci592@gmail.com>
2020-02-15 16:28:52 +0200

    Revert "Revert "Revert "added improvements to the authentication"""
    This reverts commit edc16105ed4a2e7fc10be24cb73a7def13229c75.

--- progress_app/lib/screens/auth_screen.dart
+++ progress_app/lib/screens/auth_screen.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/auth.dart';
-import '../models/http_exception.dart';
-
 
 enum AuthMode { Signup, Login }
 
@@ -21,6 +19,19 @@ class AuthScreen extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromRGBO(255, 255, 255, 1),
+                  //  Color.fromRGBO(46,139,87, 1).withOpacity(0.9),
+                ],
+                begin: Alignment.topRight,
+                end: Alignment.bottomRight,
+                stops: [0, 1],
+              ),
+            ),
+          ),
+          SingleChildScrollView(
             child: Container(
               height: deviceSize.height,
               width: deviceSize.width,
@@ -28,7 +39,23 @@ class AuthScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  Container(        
+                  Flexible(
+                    child: Container(
+                      child: Text(
+                        'Login',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          color:
+                              Colors.black,
+                          fontSize: 40,
+                          fontFamily: 'Segoe UI',
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: deviceSize.width > 600 ? 2 : 1,
                     child: AuthCard(),
                   ),
                 ],
@@ -60,25 +87,7 @@ class _AuthCardState extends State<AuthCard> {
   var _isLoading = false;
   final _passwordController = TextEditingController();
 
-  void _showErrorDialog(String message) {		
-    showDialog(		
-      context: context,		
-      builder: (ctx) => AlertDialog(		
-            title: Text('An Error Occurred!'),		
-            content: Text(message),		
-            actions: <Widget>[		
-              FlatButton(		
-                child: Text('Okay'),		
-                onPressed: () {		
-                  Navigator.of(ctx).pop();		
-                },		
-              )		
-            ],		
-          ),		
-    );		
-  }
-
-   Future<void> _submit() async {
+  Future<void> _submit() async {
     if (!_formKey.currentState.validate()) {
       // Invalid!
       return;
@@ -87,38 +96,18 @@ class _AuthCardState extends State<AuthCard> {
     setState(() {
       _isLoading = true;
     });
-    try {
-      if (_authMode == AuthMode.Login) {
-        // Log user in
-        await Provider.of<Auth>(context, listen: false).login(
-          _authData['email'],
-          _authData['password'],
-        );
-      } else {
-        // Sign user up
-        await Provider.of<Auth>(context, listen: false).signup(
-          _authData['email'],
-          _authData['password'],
-        );
-      }
-    } on HttpException catch (error) {
-      var errorMessage = 'Authentication failed';
-      if (error.toString().contains('EMAIL_EXISTS')) {
-        errorMessage = 'This email address is already in use.';
-      } else if (error.toString().contains('INVALID_EMAIL')) {
-        errorMessage = 'This is not a valid email address';
-      } else if (error.toString().contains('WEAK_PASSWORD')) {
-        errorMessage = 'This password is too weak.';
-      } else if (error.toString().contains('EMAIL_NOT_FOUND')) {
-        errorMessage = 'Could not find a user with that email.';
-      } else if (error.toString().contains('INVALID_PASSWORD')) {
-        errorMessage = 'Invalid password.';
-      }
-      _showErrorDialog(errorMessage);
-    } catch (error) {
-      const errorMessage =
-          'Could not authenticate you. Please try again later.';
-      _showErrorDialog(errorMessage);
+    if (_authMode == AuthMode.Login) {
+      // Log user in
+      await Provider.of<Auth>(context, listen: false).login(
+        _authData['email'],
+        _authData['password'],
+      );
+    } else {
+      // Sign user up
+      await Provider.of<Auth>(context, listen: false).signup(
+        _authData['email'],
+        _authData['password'],
+      );
     }
     setState(() {
       _isLoading = false;
@@ -140,133 +129,107 @@ class _AuthCardState extends State<AuthCard> {
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
-    return  Container(
+    return Card(
+      child: Container(
+        height: _authMode == AuthMode.Signup ? 540 : 380,
+        constraints:
+            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 540 : 380),
         width: deviceSize.width,
         padding: EdgeInsets.all(26.0),
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-                    width: double.infinity,
-                    child: Container(
-                      padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
-                      child: Text(
-                          _authMode == AuthMode.Login ? 'Log in' : 'Sign up',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(         
-                              color: Color.fromRGBO(50, 50, 50, 1),
-                              fontSize:40,
-                              fontFamily: 'Segoe'
-                              )),
-                    ),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'E-Mail',
+                     ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value.isEmpty || !value.contains('@')) {
+                      return 'Invalid email!';
+                    }
+                  },
+                  onSaved: (value) {
+                    _authData['email'] = value;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                  controller: _passwordController,
+                  validator: (value) {
+                    if (value.isEmpty || value.length < 5) {
+                      return 'Password is too short!';
+                    }
+                  },
+                  onSaved: (value) {
+                    _authData['password'] = value;
+                  },
+                ),
+                if (_authMode == AuthMode.Signup)
+                  TextFormField(
+                    enabled: _authMode == AuthMode.Signup,
+                    decoration: InputDecoration(labelText: 'Confirm Password'),
+                    obscureText: true,
+                    validator: _authMode == AuthMode.Signup
+                        ? (value) {
+                            if (value != _passwordController.text) {
+                              return 'Passwords do not match!';
+                            }
+                          }
+                        : null,
                   ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'E-Mail',    
-              ),
-              style: TextStyle(         
-                              fontSize:18,
-                              fontFamily: 'Segoe'
-                              ),
-              keyboardType: TextInputType.emailAddress,
-              validator: (value) {
-                if (value.isEmpty || !value.contains('@')) {
-                  return 'Invalid email!';
-                }
-              },
-              onSaved: (value) {
-                _authData['email'] = value;
-              },
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-              style: TextStyle(         
-                              fontSize:18,
-                              fontFamily: 'Segoe'
-                              ),
-              controller: _passwordController,
-              validator: (value) {
-                if (value.isEmpty || value.length < 5) {
-                  return 'Password is too short!';
-                }
-              },
-              onSaved: (value) {
-                _authData['password'] = value;
-              },
-            ),
-            if (_authMode == AuthMode.Signup)
-              TextFormField(
-                enabled: _authMode == AuthMode.Signup,
-                decoration: InputDecoration(labelText: 'Confirm Password'),
-                obscureText: true,
-                style: TextStyle(         
-                              fontSize:18,
-                              fontFamily: 'Segoe'
-                              ),
-                validator: _authMode == AuthMode.Signup
-                    ? (value) {
-                        if (value != _passwordController.text) {
-                          return 'Passwords do not match!';
-                        }
-                      }
-                    : null,
-              ),
-            SizedBox(
-              height: 50,
-            ),
-            if (_isLoading)
-              CircularProgressIndicator()
-            else
-              Container(
-                height: 50.0,
-                child: RaisedButton(
-                  onPressed: _submit,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0)),
-                  padding: EdgeInsets.all(0.0),
-                  child: Ink(
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Color.fromRGBO(111, 198, 25, 1),
-                            Color.fromRGBO(159, 235, 83, 1)
-                          ],
-                          begin: Alignment.centerLeft,
-                          end: Alignment.centerRight,
+                SizedBox(
+                  height: 20,
+                ),
+                if (_isLoading)
+                  CircularProgressIndicator()
+                else
+                  Container(
+                    height: 50.0,
+                    child: RaisedButton(
+                      onPressed: _submit,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                      padding: EdgeInsets.all(0.0),
+                      child: Ink(
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color.fromRGBO(111,198,25,1), 
+                                 Color.fromRGBO(159, 235, 83, 1)],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                            ),
+                            borderRadius: BorderRadius.circular(10.0)),
+                        child: Container(
+                          constraints:
+                              BoxConstraints(maxWidth: 400.0, minHeight: 50.0),
+                          alignment: Alignment.center,
+                          child:  Text(_authMode == AuthMode.Login ? 'Log in' : 'Sign up',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white),
+                          ),
                         ),
-                        borderRadius: BorderRadius.circular(10.0)),
-                    child: Container(
-                      constraints:
-                          BoxConstraints(maxWidth: 400.0, minHeight: 50.0),
-                      alignment: Alignment.center,
-                      child: Text(
-                        _authMode == AuthMode.Login ? 'Log in' : 'Sign up',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(       
-                              color: Colors.white, 
-                              fontSize:20,
-                              fontFamily: 'Segoe'
-                              ),
                       ),
                     ),
                   ),
+
+                FlatButton(
+                  child: Text(
+                      '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
+                  onPressed: _switchAuthMode,
+                  padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 4),
+                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  textColor: Color.fromRGBO(146, 139, 87, 1),
                 ),
-              ),
-            FlatButton(
-              child: Text(
-                  '${_authMode == AuthMode.Login ? 'Sign up' : 'Log in'} Instead',
-                   style: TextStyle(         
-                              color: Colors.black,
-                              fontSize:18,
-                              fontFamily: 'Segoe'),),
-              onPressed: _switchAuthMode,
-              padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-              textColor: Color.fromRGBO(0, 0, 0, 1),
+              ],
             ),
-          ],
+          ),
         ),
-      )
-    ;
+      ),
+    );
   }
 }
